3136774f66d4ef371d2c33d27b0037f065d67b79
mayursawaleggsp <mayursawale.ggsp@gmail.com>
2025-11-08 12:53:19 +0530

    Update and rename case2.java to hollowSquare_pattern.java

--- hollowSquare_pattern.java
+++ hollowSquare_pattern.java
@@ -1,4 +1,4 @@
-public class case2 {
+public class hollowSquare_pattern {
     public static void main(String[] args) {
         int rows = 5;
         int cols = 5;
@@ -21,4 +21,5 @@ public class case2 {
             System.out.println();
         }
     }
+
 }
